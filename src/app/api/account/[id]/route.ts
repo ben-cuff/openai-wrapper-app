4da5b8bb2b826e16f6d8ceecab3638856c898271
ben-cuff <bencuff14@gmail.com>
2025-02-08 15:54:24 -0500

    made it delete from conversation too in the delete account route

--- src/app/api/account/[id]/route.ts
+++ src/app/api/account/[id]/route.ts
@@ -6,16 +6,20 @@ export async function DELETE(req: Request) {
 		const segments = url.pathname.split("/");
 		const id = Number(segments[segments.length - 1]);
 
-		const deletedAccount = await prismaLib.account.delete({
-			where: { id },
+		const deletedAccount = await prismaLib.$transaction(async (prisma) => {
+			await prisma.conversation.deleteMany({
+				where: { userId: id },
+			});
+			return await prisma.account.delete({
+				where: { id },
+			});
 		});
 
-        if (!deletedAccount) {
-            return new Response(
-				JSON.stringify({ error: "User not found" }),
-				{ status: 404 }
-			);
-        }
+		if (!deletedAccount) {
+			return new Response(JSON.stringify({ error: "User not found" }), {
+				status: 404,
+			});
+		}
 
 		return new Response(JSON.stringify(deletedAccount), { status: 200 });
 	} catch (error) {
